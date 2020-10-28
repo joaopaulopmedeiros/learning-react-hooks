a20b843028116624b02fffec2c8e4b6d566e7050
joao paulo <joaopaulopmedeiros@gmail.com>
2020-10-28 13:31:56 -0300

    setting up project

---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./style.css";
+
+const Home = () => {
+  return (
+    <>
+      <header>
+        <h1>Learning React Hooks</h1>
+      </header>
+      <main>
+          <h2>useState</h2>
+      </main>
+    </>
+  );
+};
+
+export default Home;
